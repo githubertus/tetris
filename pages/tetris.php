487a6e3fb27be5a5bd034e0079eaa825b946154e
Eric <ctlui@users.noreply.github.com>
2015-08-14 08:10:48 +0200

    Update tetris.php

--- pages/tetris.php
+++ pages/tetris.php
@@ -1,2 +1,3 @@
-<iframe src='components/tetris/tetris/index.html' width="1000" height="680" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
+<div>
+</div><iframe src='components/tetris/tetris/index.html' width="1000" height="680" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
 </div>
