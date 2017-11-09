82c0408fb1c8c48694a75118ef23de37428af87a
githubertus <githubertus@ongolito.net>
2017-11-09 18:57:04 +0100

    make register_sections_menu Ossn 4x compatible

--- ossn_com.php
+++ ossn_com.php
@@ -10,23 +10,19 @@
  */
 
 function ossn_tetris_init() {
-	ossn_register_page('tetris', 'ossn_tetris_pages');
-	ossn_extend_view('css/ossn.default', 'tetris/css/tetris');
 	if (ossn_isLoggedin()) {       
-    	ossn_register_sections_menu('newsfeed', array(
+		ossn_register_page('tetris', 'ossn_tetris_pages');
+		ossn_extend_view('css/ossn.default', 'tetris/css/component');
+    		ossn_register_sections_menu('newsfeed', array(
 		'name' => 'tetris',
         	'text' => ossn_print('com:ossn:tetris'),
         	'url' => ossn_site_url('tetris'),
-		'section' => 'links',
-		'icon' => true
+		'section' => 'links'
 	    ));		
     }
 }
 
 function ossn_tetris_pages($pages) {
-	if (!ossn_isLoggedin()) {
-		ossn_error_page();
-	}
 	$title = ossn_print('com:ossn:tetris');
 	$contents['content'] = ossn_plugin_view('tetris/pages/tetris');
 	$content = ossn_set_page_layout('contents', $contents);
